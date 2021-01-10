3d6d21ec13edd3b44fd95e47977b3b95db9a74ca
himanshunete <himanshuneteh@gmail.com>
2021-01-10 17:26:19 +0530

    deleted

--- SnakeLadderGameUC1.java
+++
@@ -1,7 +0,0 @@
-public class  SnakeLadderGameUC1{
-   public static void main(String[] args){
-      int numberOfPlayers = 1;
-      int position = 0;
-      System.out.println( " Only " + numberOfPlayers + " player is playing the game at position " + position + " now "  ) ;
-   } 
-}
