1ab6bb6d5610c41088b194625d7e13d0844f1428
himanshunete <himanshuneteh@gmail.com>
2021-01-10 17:04:48 +0530

    ADDED: functionality to print the message that single player is playing

---
+++ SnakeLadderGameUC1.java
@@ -0,0 +1,7 @@
+public class  SnakeLadderGameUC1{
+   public static void main(String[] args){
+      int numberOfPlayers = 1;
+      int position = 0;
+      System.out.println( " Only " + numberOfPlayers + " player is playing the game at position " + position + " now "  ) ;
+   } 
+}
